ec2e1e78c8b09232f1115960cd844ddc43e07c88
dncmaduro <manhdungtran.vnuuet@gmail.com>
2025-10-15 17:16:49 +0700

    feat: tags

--- src/database/schemas/TaskTag.ts
+++ src/database/schemas/TaskTag.ts
@@ -5,13 +5,15 @@ export interface TaskTag extends Document {
   color?: string
   createdAt: Date
   updatedAt: Date
+  deletedAt?: Date | null
 }
 
 export const TaskTagSchema = new Schema<TaskTag>({
   name: { type: String, required: true, trim: true, unique: true },
   color: { type: String, trim: true },
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  updatedAt: { type: Date, default: Date.now },
+  deletedAt: { type: Date, default: null }
 })
 
 export const TaskTagModel = model<TaskTag>("TaskTag", TaskTagSchema)
