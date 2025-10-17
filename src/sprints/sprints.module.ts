788536b296af22b6af0eff6e235471b190df5797
Tran Manh Dung <manhdungtran.vnuuet@gmail.com>
2025-10-17 14:56:35 +0700

    feat: current sprint (#8)

--- src/sprints/sprints.module.ts
+++ src/sprints/sprints.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from "@nestjs/mongoose"
 import { SprintsController } from "./sprints.controller"
 import { SprintsService } from "./sprints.service"
 import { SprintSchema } from "../database/schemas/Sprint"
+import { TaskSchema } from "../database/schemas/Task"
 import { JwtModule } from "@nestjs/jwt"
 import { AuthModule } from "../auth/auth.module"
 import { RoleUserSchema } from "../database/schemas/RoleUser"
@@ -11,6 +12,7 @@ import { RoleUserSchema } from "../database/schemas/RoleUser"
   imports: [
     MongooseModule.forFeature([
       { name: "Sprint", schema: SprintSchema },
+      { name: "Task", schema: TaskSchema },
       { name: "RoleUser", schema: RoleUserSchema }
     ]),
     JwtModule,
